5dc41e5677c811b64d62a8003aeaa5ead3845041
Théotime <31313380+Tronaidev@users.noreply.github.com>
2023-03-01 16:12:50 -0500

    Update episode1.asm

--- episode1.asm
+++ episode1.asm
@@ -2,4 +2,9 @@
    message: .asciiz "Hello World !"
   
 .text
+   li $v0, 4         # On annonce que l'on affiche un string.
+   la $a0, message   # On donne le string à afficher.
+   syscall           # On demande au système d'effectuer l'action.
    
+   li $v0, 10        # On annonce que l'on va arreter le programme.
+   syscall           # On demande au système d'effectuer l'action.
